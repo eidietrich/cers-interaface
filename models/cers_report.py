12cac6c369959658be6221a5583e769872c8fd9d
eidietrich <eric.dietrich@gmail.com>
2024-08-05 11:33:01 -0600

    Aug 2 content update

--- models/cers_report.py
+++ models/cers_report.py
@@ -453,7 +453,7 @@ class Report:
             }
 
             session = requests.Session()
-            p = session.post(post_url, post_payload, timeout=120)
+            p = session.post(post_url, post_payload, timeout=180)
             if 'fileName' in p.json():
                 r = session.get(get_url, params=p.json())
                 if r.text == '':
